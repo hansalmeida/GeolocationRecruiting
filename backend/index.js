a6f6d38c1fe87c4ca99ed57a1dd90623e7f98f2d
Hans Almeida <hansalmeida@hotmail.com>
2020-01-27 20:33:36 -0300

    Express configured to use JSON
    Routes requests explained in comments

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,17 @@ const express = require("express")
 
 const app = express()
 
-// Routes
+// This will make Express understand JSON data.
+app.use(express.json())
+
+/**
+ * Routes
+ *
+ * Can receive different kinds of requests
+ * request.query (localhost/route?query=value)
+ * request.params (localhost/:id/ == localhost/1/)
+ * request.body usually info sent from a <form>
+ */
 app.get("/", (request, response) => {
   return response.json({ message: "ハンスです！宜しくお願い致します！" })
 })
