ddbb48d8fdb66f54edb3a43618f425211ff64998
Hans Almeida <hansalmeida@hotmail.com>
2020-01-27 20:48:24 -0300

    Routes setup done
    Routes moved to a separate file "routes.js"

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const routes = require("./routes")
+
+const app = express()
+
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@omnistack-b22jm.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+)
+
+// This will make Express understand JSON data.
+app.use(express.json())
+
+// This will load all routes on "routes.js" file
+app.use(routes)
+
+// Port that express will be listening to
+app.listen(3333)
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,6 @@
-const express = require("express")
+const { Router } = require("express")
 
-const app = express()
-
-// This will make Express understand JSON data.
-app.use(express.json())
+const route = Router()
 
 /**
  * Routes
@@ -13,9 +10,8 @@ app.use(express.json())
  * request.params (localhost/:id/ == localhost/1/)
  * request.body usually info sent from a <form>
  */
-app.get("/", (request, response) => {
+route.get("/", (request, response) => {
   return response.json({ message: "ハンスです！宜しくお願い致します！" })
 })
 
-// Port that express will be listening to
-app.listen(3333)
+module.exports = route
