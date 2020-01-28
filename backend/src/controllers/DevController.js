af7698f56fa49a02aea390be1dfe807d885a441c
Hans Almeida <hansalmeida@hotmail.com>
2020-01-27 21:36:48 -0300

    Returns user instance if it already exists

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -5,32 +5,37 @@ module.exports = {
   async store(request, response) {
     const { githubUsername, techs, latitude, longitude } = request.body
 
-    const apiResponse = await axios.get(
-      `https://api.github.com/users/${githubUsername}`
-    )
+    let dev = await Dev.findOne({ githubUsername })
 
-    /**
-     * Destructure some data returned from Github's API
-     * If there's no name from the data received since they're optional, the default will be the login
-     */
-    const { name = login, bio, avatar_url } = apiResponse.data
+    if (!dev) {
+      // Gets user data from Github
+      const apiResponse = await axios.get(
+        `https://api.github.com/users/${githubUsername}`
+      )
 
-    const techsArray = techs.split(",").map(tech => tech.trim())
+      /**
+       * Destructure some data returned from Github's API
+       * If there's no name from the data received since they're optional, the default will be the login
+       */
+      const { name = login, bio, avatar_url } = apiResponse.data
 
-    // Longitude must always come before Latitude to be properly saved in mongoDb
-    const location = {
-      type: "Point",
-      coordinates: [longitude, latitude]
+      const techsArray = techs.split(",").map(tech => tech.trim())
+
+      // Longitude must always come before Latitude to be properly saved in mongoDb
+      const location = {
+        type: "Point",
+        coordinates: [longitude, latitude]
+      }
+
+      dev = await Dev.create({
+        githubUsername,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location
+      })
     }
-
-    const dev = await Dev.create({
-      githubUsername,
-      name,
-      avatar_url,
-      bio,
-      techs: techsArray,
-      location
-    })
 
     return response.json(dev)
   }
