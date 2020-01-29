6f8d12075706766ff21b3e31cd454a20c52ec793
Hans Almeida <hansalmeida@hotmail.com>
2020-01-29 16:35:19 -0300

    Form logic added
    Now it can filter the users by their technology as passed in the input

--- mobile/src/pages/Main.jsx
+++ mobile/src/pages/Main.jsx
@@ -5,8 +5,12 @@ import { requestPermissionsAsync, getCurrentPositionAsync } from "expo-location"
 import { TextInput, TouchableOpacity } from "react-native-gesture-handler"
 import { MaterialIcons } from "@expo/vector-icons"
 
+import api from "../services/api"
+
 export default function Main({ navigation }) {
+  const [devs, setDevs] = useState([])
   const [currentRegion, setCurrentRegion] = useState(null)
+  const [techs, setTechs] = useState("")
 
   useEffect(() => {
     async function loadInitialPosition() {
@@ -27,36 +31,64 @@ export default function Main({ navigation }) {
     loadInitialPosition()
   }, [])
 
+  async function loadDevs() {
+    const { latitude, longitude } = currentRegion
+
+    const response = await api.get("/search", {
+      params: {
+        latitude,
+        longitude,
+        techs
+      }
+    })
+
+    setDevs(response.data.devs)
+  }
+
+  function handleRegionChanged(region) {
+    setCurrentRegion(region)
+  }
+
   if (!currentRegion) {
     return null
   }
 
   return (
     <>
-      <MapView initialRegion={currentRegion} style={styles.map}>
-        <Marker coordinate={{ latitude: -12.934351, longitude: -38.4149727 }}>
-          <Image
-            style={styles.avatar}
-            source={{
-              uri: "https://avatars3.githubusercontent.com/u/42522040?s=460&v=4"
-            }}
-          />
-          <Callout
-            onPress={() => {
-              navigation.navigate("Profile", { githubUsername: "Snahier" })
+      <MapView
+        onRegionChangeComplete={handleRegionChanged}
+        initialRegion={currentRegion}
+        style={styles.map}
+      >
+        {devs.map(dev => (
+          <Marker
+            key={dev._id}
+            coordinate={{
+              longitude: dev.location.coordinates[0],
+              latitude: dev.location.coordinates[1]
             }}
           >
-            <View style={styles.callout}>
-              <Text style={styles.devName}>Hans Almeida</Text>
-              <Text style={styles.bio}>
-                CTO no desemprego, etc, etc, etc...
-              </Text>
-              <Text style={styles.devTechs}>
-                ReactJS, React Native, Node.js
-              </Text>
-            </View>
-          </Callout>
-        </Marker>
+            <Image
+              style={styles.avatar}
+              source={{
+                uri: dev.avatarUrl
+              }}
+            />
+            <Callout
+              onPress={() => {
+                navigation.navigate("Profile", {
+                  githubUsername: dev.githubUsername
+                })
+              }}
+            >
+              <View style={styles.callout}>
+                <Text style={styles.devName}>{dev.name}</Text>
+                <Text style={styles.bio}>{dev.bio}</Text>
+                <Text style={styles.devTechs}>{dev.techs.join(", ")}</Text>
+              </View>
+            </Callout>
+          </Marker>
+        ))}
       </MapView>
       <View style={styles.searchForm}>
         <TextInput
@@ -65,8 +97,14 @@ export default function Main({ navigation }) {
           placeholderTextColor="#999"
           autoCapitalize="words"
           autoCorrect={false}
+          onChangeText={setTechs}
         />
-        <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+        <TouchableOpacity
+          onPress={() => {
+            loadDevs()
+          }}
+          style={styles.loadButton}
+        >
           <MaterialIcons name="my-location" size={20} color="#FFF" />
         </TouchableOpacity>
       </View>
